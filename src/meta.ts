7c3be18b7102fc6fb2acbdce38d0929e0a5463d8
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2026-07-12 09:16:03 -0700

    (docs): update README.md

--- src/meta.ts
+++ src/meta.ts
@@ -1,4 +1,4 @@
 /** Project-wide identity constants. */
 export const NAME = "honeyprompt";
-export const VERSION = "0.1.0";
-export const REPO_URL = "https://github.com/honeyprompt/honeyprompt";
+export const VERSION = "0.1.2";
+export const REPO_URL = "https://github.com/alectrocute/honeyprompt";
