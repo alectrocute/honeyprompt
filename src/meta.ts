a792be93393c1956bbef8c6dbaf67764277b6d76
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2026-07-12 10:42:36 -0700

    (docker): release 0.1.4

--- src/meta.ts
+++ src/meta.ts
@@ -1,4 +1,4 @@
 /** Project-wide identity constants. */
 export const NAME = "honeyprompt";
-export const VERSION = "0.1.3";
+export const VERSION = "0.1.4";
 export const REPO_URL = "https://github.com/alectrocute/honeyprompt";
